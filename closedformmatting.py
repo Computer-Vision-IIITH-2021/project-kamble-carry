1a3c8fbe56bf437a0d1deb873ddf68709adac6f9
2Shabby <shaikmohdshahbaz@gmail.com>
2021-03-16 02:47:58 +0530

    Revert

--- closedformmatting.py
+++ closedformmatting.py
@@ -19,7 +19,27 @@ def getlaplacian1(i_arr, consts, epsilon=1e-5, win_rad=1):
     col_inds = np.zeros(tlen)
     vals = np.zeros(tlen)
     l = 0
+    for j in range(win_rad, w - win_rad):
+        for i in range(win_rad, h - win_rad):
+            if consts[i, j]:
+                continue
+            win_inds = indsM[i - win_rad:i + win_rad + 1, j - win_rad: j + win_rad + 1]
+            win_inds = win_inds.ravel(order='F')
+            win_i = i_arr[i - win_rad:i + win_rad + 1, j - win_rad: j + win_rad + 1, :]
+            win_i = win_i.reshape((neb_size, c), order='F')
+            win_mu = np.mean(win_i, axis=0).reshape(c, 1)
+            win_var = np.linalg.inv(
+                np.matmul(win_i.T, win_i) / neb_size - np.matmul(win_mu, win_mu.T) + epsilon / neb_size * np.identity(
+                    c))
 
+            win_i2 = win_i - np.repeat(win_mu.transpose(), neb_size, 0)
+            tvals = (1 + np.matmul(np.matmul(win_i2, win_var), win_i2.T)) / neb_size
+
+            ind_mat = np.broadcast_to(win_inds, (neb_size, neb_size))
+            row_inds[l: (neb_size ** 2 + l)] = ind_mat.ravel(order='C')
+            col_inds[l: neb_size ** 2 + l] = ind_mat.ravel(order='F')
+            vals[l: neb_size ** 2 + l] = tvals.ravel(order='F')
+            l += neb_size ** 2
 
 def getLaplacian(img):
     h, w, _ = img.shape
