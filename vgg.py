5e37e17a7b7e45ef01eacf98ad586629c1d3f2b0
Nikhil E <42788204+ENikhil@users.noreply.github.com>
2021-04-15 03:33:43 +0530

    Update vgg.py

--- vgg.py
+++ vgg.py
@@ -3,3 +3,12 @@ import tensorflow as tf
 import numpy as np
 import time
 import inspect
+
+class Vgg19:
+    def __init__(self, vgg19_npy_path=None):
+        if vgg19_npy_path is None:
+            path = inspect.getfile(Vgg19)
+            path = os.path.abspath(os.path.join(path, os.pardir))
+            path = os.path.join(path, "vgg19.npy")
+            vgg19_npy_path = path
+        self.data_dict = np.load(vgg19_npy_path, encoding='latin1', allow_pickle=True).item()
