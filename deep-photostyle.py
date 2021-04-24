921c4bd42fe76d1112a8a42a907e273d2d83ea50
2Shabby <shaikmohdshahbaz@gmail.com>
2021-04-24 16:56:28 +0530

    Initial2

--- deep-photostyle.py
+++ deep-photostyle.py
@@ -10,3 +10,19 @@ parser.add_argument("--content_image_path", dest='content_image_path',  nargs='?
                     help="Path to the content image")
 parser.add_argument("--style_image_path",   dest='style_image_path',    nargs='?',
                     help="Path to the style image")
+parser.add_argument("--content_seg_path",   dest='content_seg_path',    nargs='?',
+                    help="Path to the style segmentation")
+parser.add_argument("--style_seg_path",     dest='style_seg_path',      nargs='?',
+                    help="Path to the style segmentation")
+parser.add_argument("--init_image_path",    dest='init_image_path',     nargs='?',
+                    help="Path to init image", default="")
+parser.add_argument("--output_image",       dest='output_image',        nargs='?',
+                    help='Path to output the stylized image', default="best_stylized.png")
+parser.add_argument("--serial",             dest='serial',              nargs='?',
+                    help='Path to save the serial out_iter_X.png', default='./')
+
+# Training Optimizer Options
+parser.add_argument("--max_iter",           dest='max_iter',            nargs='?', type=int,
+                    help='maximum image iteration', default=1000)
+parser.add_argument("--learning_rate",      dest='learning_rate',       nargs='?', type=float,
+                    help='learning rate for adam optimizer', default=1.0)
